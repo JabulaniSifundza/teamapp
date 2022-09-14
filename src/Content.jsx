2a4d9a43177387c0aaf6515466cedaf0c567594d
Jabulani Sifundza <jjsifundza@gmail.com>
2022-09-14 11:14:45 +0200

    Up to card highlight/bordering

---
+++ src/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Content = () =>{
+	return (
+		<main>
+		<h1>Content</h1>
+		</main>	 
+	)
+}
+
+export default Content;
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () =>{
+	return (
+		<header className="mt-4 p5 bg-primary text-white rounded">
+		<h1>Header</h1>
+		</header>	 
+	)
+}
+export default Header;
