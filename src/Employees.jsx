da4cdf37291a4fd8d7af5752e67578f1f74aabde
Jabulani Sifundza <jjsifundza@gmail.com>
2022-09-15 08:14:59 +0200

    Updated feature set

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,106 +1,8 @@
-import {useState} from 'react';
+
 import femaleProfile from './images/femaleProfile.jpg';
 import maleProfile from './images/maleProfile.jpg';
 
-const Employees = () =>{
-
-	const [selectedTeam, setTeam] = useState("TeamA");
-
-
-	const [employees, setEmployees] = useState([{
-		id: 1,
-		fullName: "Bob Jones",
-		designation: "JavaScript Developer",
-		gender: "male",
-		teamName: "TeamA"
-	  },
-	  {
-		id: 2,
-		fullName: "Jill Bailey",
-		designation: "Node Developer",
-		gender: "female",
-		teamName: "TeamA"
-	  },
-	  {
-		id: 3,
-		fullName: "Gail Shepherd",
-		designation: "Java Developer",
-		gender: "female",
-		teamName: "TeamA"
-	  },
-	  {
-		id: 4,
-		fullName: "Sam Reynolds",
-		designation: "React Developer",
-		gender: "male",
-		teamName: "TeamB"
-	  },
-	  {
-		id: 5,
-		fullName: "David Henry",
-		designation: "DotNet Developer",
-		gender: "male",
-		teamName: "TeamB"
-	  },
-	  {
-		id: 6,
-		fullName: "Sarah Blake",
-		designation: "SQL Server DBA",
-		gender: "female",
-		teamName: "TeamB"
-	  },
-	  {
-		id: 7,
-		fullName: "James Bennet",
-		designation: "Angular Developer",
-		gender: "male",
-		teamName: "TeamC"
-	  },
-	  {
-		id: 8,
-		fullName: "Jessica Faye",
-		designation: "API Developer",
-		gender: "female",
-		teamName: "TeamC"
-	  },
-	  {
-		id: 9,
-		fullName: "Lita Stone",
-		designation: "C++ Developer",
-		gender: "female",
-		teamName: "TeamC"
-	  },
-	  {
-		id: 10,
-		fullName: "Daniel Young",
-		designation: "Python Developer",
-		gender: "male",
-		teamName: "TeamD"
-	  },
-	  {
-		id: 11,
-		fullName: "Adrian Jacobs",
-		designation: "Vue Developer",
-		gender: "male",
-		teamName: "TeamD"
-	  },
-	  {
-		id: 12,
-		fullName: "Devin Monroe",
-		designation: "Graphic Designer",
-		gender: "male",
-		teamName: "TeamD"
-	  }]);
-
-	  function handleTeamSelectionChange(event){
-			setTeam(event.target.value);
-	  };
-
-	  function handleEmployeeCardClick (event){
-		const transformedEmployees = employees.map((employee)=> employee.id === parseInt(event.currentTarget.id) ? (employee.teamName === selectedTeam) ? {...employee, teamName:''} : {...employee, teamName: selectedTeam} : employee);
-		setEmployees(transformedEmployees);
-	  }
-
+const Employees = ({employees, selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange}) =>{
 	return (
 		<main className="container">
 		<div className="row justify-content mt-3 mb-3">
@@ -114,10 +16,6 @@ const Employees = () =>{
 			</div>
 		
 		</div>
-
-
-
-
 			<div className="row justify-content-center mt-3 mb-3">
 			<div className="column">
 			<div className="card-collection">
@@ -125,7 +23,7 @@ const Employees = () =>{
 		{
 			employees.map((employee) => {
 			return(
-				<div id={employee.id} className={(employee.teamName === selectedTeam ? "card m-2 standout" : "card m-2")} style={{cursor:"pointer"}} onClick={handleEmployeeCardClick}>
+				<div key={employee.id} id={employee.id} className={(employee.teamName === selectedTeam ? "card m-2 standout" : "card m-2")} style={{cursor:"pointer"}} onClick={handleEmployeeCardClick}>
 				{(employee.gender === 'male') ? <img src={maleProfile} alt="Employee" className="card-img-top"/> : <img src={femaleProfile} alt="Employee" className="card-img-top"/>}
 				<div className="card-body">
 					<h5 className="card-title">Full Name: {employee.fullName}</h5>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+var today = new Date();
 
 
 const Footer = () =>{
 	return (
-		<footer>
-		<h1>Footer</h1>
+		<footer className="container">
+		<div className="row justify-content-center mt-3 mb-4">
+			<div className="col-8">
+				<h4>Team Memeber Allocation App - {today.getFullYear()}</h4>
+			</div>
+		</div>
 		</footer>	 
 	)
 }
